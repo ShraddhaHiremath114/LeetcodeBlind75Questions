8b7dedb5e4ec48deb5c325a563865145b53e3e76
shraddhahiremath114 <shraddhahiremath114@gmail.com>
2024-12-10 20:56:13 +0530

    Merge

---
+++ MergeSortedArray.java
@@ -0,0 +1,29 @@
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] a={1,3,5};
+        int[] b={2,4,6};
+        int n=a.length;
+        int m=b.length;
+        int i1=0,i2=0;
+        int i=0;
+        
+        int ans[] = new int[n+m];
+        while(i1<n && i2<m){
+            if(a[i1]<b[i2]){
+                ans[i++]=a[i1++];
+            }else{
+                ans[i++]=b[i2++];
+            }
+        }
+        while(i1<n){
+            ans[i++]=a[i1++];
+        }
+        while (i2<m) {
+            ans[i++]=b[i2++];
+        }
+
+        for(int val:ans){
+            System.out.print(val+" ");
+        }
+    }
+}
